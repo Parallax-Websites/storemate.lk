03305079a9fdecbf33cdc047c2c8b31e2bcebcd2
kavindurs8 <kavindurs8@gmail.com>
2025-10-28 12:52:33 +0530

    new m

--- resources/js/Components/Hero.jsx
+++ resources/js/Components/Hero.jsx
@@ -158,7 +158,7 @@ export default function Hero() {
                                     {/* Main content container */}
                                     <div className="relative p-[20px] rounded-3xl ">
                                         <img
-                                            src="https://cimacleaners.com.au/wp-content/uploads/2025/10/Frame-2-1-1-scaled.webp"
+                                            src="https://cimacleaners.com.au/wp-content/uploads/2025/10/Group-7-1.png"
                                             alt="Storemate OMS Dashboard"
                                             className="relative w-full transform transition-all duration-700 hover:scale-[1.03]"
                                         />
